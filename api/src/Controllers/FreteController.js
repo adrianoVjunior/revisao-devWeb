1db9a242bbe59be2c77e918c4e96e05ecc22fa24
Adriano Júnior <adriano753@live.com>
2020-04-14 13:50:19 -0300

    finish

--- api/src/Controllers/FreteController.js
+++ api/src/Controllers/FreteController.js
@@ -1,7 +1,16 @@
 module.exports = {
     async post(request, response) {
-        let { comprimento, largura, altura } = request.body
+        let { comprimento, largura, altura, valorMercadoria } = request.body
         var valorFrete = ((comprimento * largura * altura) / 6000)
-        return response.json({ "valorFrete": valorFrete })
+        var taxaRisco = ((valorMercadoria / 100) * 0.3)
+        return response.json({
+            "Frete": {
+                "PAC": valorFrete,
+                "Sedex": valorFrete * 10,
+                "Sedex 10": valorFrete * 20,
+                "Sedex 12": valorFrete * 30
+            },
+            "Taxa Risco": valorFrete + taxaRisco
+        })
     }
 }
